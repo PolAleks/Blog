1e89d88956c1683ff03c634449a8df15fb905ff7
AleksHome <aa.polushkin@yandex.ru>
2023-12-10 12:00:34 +0300

    Добавьте файлы проекта.

---
+++ Blog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        // Внешний ключ
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+
+        // Внешний ключ
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
